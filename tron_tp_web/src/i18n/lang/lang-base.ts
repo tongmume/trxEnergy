e6ac37a8d245ae67dfbceee5de865b707e5224aa
tongmume <mingmingz925@gmail.com>
2024-09-07 11:20:30 +0800

    Add files via upload

---
+++ tron_tp_web/src/i18n/lang/lang-base.ts
@@ -0,0 +1,21 @@
+export type langType = {
+  title: string;
+  tabbar: {
+    home: string;
+    list: string;
+    member: string;
+    demo: string;
+  };
+  language: {
+    en: string;
+    zh: string;
+  };
+  introduction: string;
+  home: {
+    support: string;
+    cssMultiLanguage: string;
+  };
+  list: {
+    details: string;
+  };
+};
